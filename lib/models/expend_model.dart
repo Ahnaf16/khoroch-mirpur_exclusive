8bee3ad90dd8cf61f1400d7c9085818620d3fae1
Ahnaf Sakil <55395335+Ahnaf16@users.noreply.github.com>
2023-05-19 19:27:54 +0600

    login fix

--- lib/models/expend_model.dart
+++ lib/models/expend_model.dart
@@ -1,9 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+enum ExpendStatus {
+  pending,
+  approved,
+  rejected;
+
+  factory ExpendStatus.fromMap(String status) => values.byName(status);
+}
+
 class ExpendModel {
   ExpendModel({
-    required this.item,
     required this.amount,
+    required this.item,
+    required this.status,
     required this.date,
   });
 
@@ -12,39 +21,16 @@ class ExpendModel {
       amount: doc['amount'] ?? 0,
       item: doc['item'] ?? '',
       date: (doc['date'] as Timestamp).toDate(),
+      status: ExpendStatus.fromMap(doc['status']),
     );
   }
 
-  final int amount;
-  final String item;
-  final DateTime date;
-
-  @override
-  String toString() => '''
-     amount: $item,
-     item: $amount,
-     date: $date
-    ''';
-
-  Map<String, dynamic> toMap() {
-    final result = <String, dynamic>{};
-
-    result.addAll({'amount': amount});
-    result.addAll({'item': item});
-    result.addAll({'date': date});
-
-    return result;
-  }
-
-  ExpendModel copyWith({
-    int? amount,
-    String? item,
-    DateTime? date,
-  }) {
+  factory ExpendModel.fromMap(Map<String, dynamic> map) {
     return ExpendModel(
-      amount: amount ?? this.amount,
-      item: item ?? this.item,
-      date: date ?? this.date,
+      amount: map['amount']?.toInt() ?? 0,
+      item: map['item'] ?? '',
+      status: ExpendStatus.fromMap(map['status']),
+      date: DateTime.fromMillisecondsSinceEpoch(map['date']),
     );
   }
 
@@ -52,5 +38,36 @@ class ExpendModel {
     amount: 0,
     item: '',
     date: DateTime.now(),
+    status: ExpendStatus.approved,
   );
+
+  final int amount;
+  final DateTime date;
+  final String item;
+  final ExpendStatus status;
+
+  Map<String, dynamic> toMap() {
+    final result = <String, dynamic>{};
+
+    result.addAll({'amount': amount});
+    result.addAll({'item': item});
+    result.addAll({'status': status.name});
+    result.addAll({'date': date.millisecondsSinceEpoch});
+
+    return result;
+  }
+
+  ExpendModel copyWith({
+    int? amount,
+    String? item,
+    ExpendStatus? status,
+    DateTime? date,
+  }) {
+    return ExpendModel(
+      amount: amount ?? this.amount,
+      item: item ?? this.item,
+      status: status ?? this.status,
+      date: date ?? this.date,
+    );
+  }
 }
